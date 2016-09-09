01da41951ca1cb239aede9a84f02d9b93d9098b5
Kevin Baxter <kbax6342@gmail.com>
2016-09-09 14:39:28 -0400

    Finished lab

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,9 +1,64 @@
+import java.util.Scanner;
+
 
 public class Factorial {
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n;
+		int result;
+		String cont;
+		
+		// Welcome the User to the Factorial Calculator
+		
+		System.out.println("Welcome to the Factorial Calculator");
+	
+		
+	
+	do{
+		
+		//Get user in input between 0 and 10
+		
+		System.out.println("Enter and integer that's greater that 0 but less than 10: ");
+			n = input.nextInt();
+			
+		
+		result = factorial(n);
+		
+		//Print the Factorial to the Console (use are for loop)	
+		
+		System.out.println("The factorial of "+ n + " is " +  result  + ".");
+		
+		System.out.println("Contintue (yes/no): ");
+			cont = input.nextLine();
+			cont = input.nextLine();
+		
+			
+	}while(cont.equalsIgnoreCase("yes"));
 
 	}
 
+	public static int factorial(int n){
+		
+		int option1;
+		int option2;
+		
+ if (n ==1){
+			option1 = 1;
+			return option1;
+		}else{
+			option2 = n * factorial(n-1);
+			return option2;
+		}
+		
+		
+		
+		
+		
+		
+	}
 }
